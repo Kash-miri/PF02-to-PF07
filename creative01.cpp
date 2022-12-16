188ebb02c5db9995f8e77fb55a4358bcdbeab481
Aleena Sheikh <kashmirisheikh499@gmail.com>
2022-12-16 09:09:58 +0500

    My Own Task

---
+++ creative01.cpp
@@ -0,0 +1,22 @@
+#include<iostream>
+using namespace std;
+main()
+{
+system("Color 0A");
+cout<<"                        ___"<<endl;
+cout<<"                       / ~ \\"<<endl;
+cout<<"                      | O O ]"<<endl;
+cout<<"                       \\ = /"<<endl;
+cout<<"                      _ | | _"<<endl;
+cout<<"                    /         \\"<<endl;
+cout<<"               #   /     _     \\"<<endl;
+cout<<"               \\ \\/ /|  /_\\  |\\ \\ "<<endl;
+cout<<"                \\  /    \\ /    | |  "<<endl;
+cout<<"                 \\/  |   -   | | |"<<endl;
+cout<<"                      \\_____/    #"<<endl;
+cout<<"                      | | | |"<<endl;
+cout<<"                      | | | |"<<endl;
+cout<<"                      [ ] [ ]"<<endl;
+cout<<"                      | | | |"<<endl;
+cout<<"                      /_| |_\\"<<endl;
+}
